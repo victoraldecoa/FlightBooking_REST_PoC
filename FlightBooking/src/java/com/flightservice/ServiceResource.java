87e3755aa3a00b2070176878082b0f946286e0cc
Victor Maia Aldecôa <victor.maia.a@gmail.com>
2013-02-08 15:00:39 +0100

    Added post and delete methods

--- FlightBooking/src/java/com/flightservice/ServiceResource.java
+++ FlightBooking/src/java/com/flightservice/ServiceResource.java
@@ -8,9 +8,11 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 
 /**
@@ -48,5 +50,25 @@ public class ServiceResource {
     @PUT
     @Consumes("application/xml")
     public void putXml(String content) {
+    }
+    
+    /**
+     * POST method for updating or creating an instance of ServiceResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @POST
+    @Consumes("application/xml")
+    public void postXml(String content) {
+    }
+    
+        /**
+     * PUT method for updating or creating an instance of ServiceResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @DELETE
+    @Consumes("application/xml")
+    public void deleteXml() {
     }
 }
