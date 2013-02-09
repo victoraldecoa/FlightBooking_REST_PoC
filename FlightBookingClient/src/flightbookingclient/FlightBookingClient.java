7cb753cdb65926b5b01256d6c4b86e2538fa3f5b
alex_geo <a.georgoudaki@gmail.com>
2013-02-09 14:56:54 +0100

    FlightBookingClient

--- FlightBookingClient/src/flightbookingclient/FlightBookingClient.java
+++ FlightBookingClient/src/flightbookingclient/FlightBookingClient.java
@@ -12,6 +12,7 @@ import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.representation.Form;
 import java.util.ArrayList;
+import java.util.Scanner;
 import javax.ws.rs.core.Response;
 
 /**
@@ -26,40 +27,67 @@ public class FlightBookingClient {
     public static void main(String[] args) {
         // get web service instance
         FlightBookingAPI service = FlightBookingAPI.getInstance();
-        
-        // Create one booking
-        BookedFlight booking = new BookedFlight();
-        booking.setDate("2013-02-08");
-        booking.setItineraryId("654");
-        
-        System.out.println("POST a new booking:");
-        ClientResponse res = service.postBooking(booking);
-        
-        BookedFlight postedBooking = res.getEntity(BookedFlight.class);
-        System.out.println(postedBooking);
-        
-        // Get the BookedFlight with id 564231
-        System.out.println("GET booking by id:");
-        BookedFlight bookingGet = service.getBookingById("564231");
-        
-        System.out.println(bookingGet);
-        
-        bookingGet.setDate("2013-05-05");
-        
-        System.out.println("PUT to update a booking (change of date)");
-        service.putBooking(bookingGet);
-        
-        ArrayList<BookedFlight> allBookings = new ArrayList<>(
-                                                   service.getBookings());
-        
-        System.out.println("GET all bookings:");
-        for (BookedFlight b : allBookings) {
-            System.out.print("    ");
-            System.out.println(b);
+
+        System.out.println("Press 1 if you wish to create a new booking.");
+        System.out.println("Press 2 if you wish to view your bookings.");
+        System.out.println("Press 3 if you wish to update one of your bookings.");
+        System.out.println("Press 4 if you wish to delete one of your bookings.");
+        System.out.println("Please enter your choice: ");
+
+        Scanner input = new Scanner(System.in);
+        int option = Integer.parseInt(input.next());
+        int bid = Integer.parseInt(input.next());
+
+        switch (option) {
+            case 1:
+                // Create a booking
+                BookedFlight booking = new BookedFlight();
+                booking.setDate("2013-02-08");
+                booking.setItineraryId("654");
+
+                System.out.println("POST a new booking:");
+                ClientResponse res = service.postBooking(booking);
+
+                BookedFlight postedBooking = res.getEntity(BookedFlight.class);
+                System.out.println(postedBooking);
+                break;
+            case 2:
+                // Get all bookings
+                System.out.println("GET all bookings:");
+                ArrayList<BookedFlight> allBookings = new ArrayList<>(
+                        service.getBookings());
+                for (BookedFlight b : allBookings) {
+                    System.out.print("    ");
+                    System.out.println(b);
+                }
+                break;
+            case 3:
+                // Update a booking
+                System.out.println("PUT to update a booking (change of date)");
+                System.out.println("Please enter the id of the booking you wish to update: ");
+                BookedFlight bookingGet = service.getBookingById(String.valueOf(bid));
+                service.putBooking(bookingGet);
+                break;
+            case 4:
+                // Delete a booking depeding on the booking id (bid)
+                System.out.println("DELETE one of your bookings booking");
+                service.getBookingById(String.valueOf(bid));
+                BookedFlight bookedFlight_temp = new BookedFlight();
+                service.deleteBooking(bookedFlight_temp);
+                break;
+
+
         }
-        
-        // Delete recently created booking
-        System.out.println("DELETE recently created booking");
-        service.deleteBooking(postedBooking);
+
+
+        // Get the BookedFlight with id 564231
+//        System.out.println("GET booking by id:");
+//       BookedFlight bookingGet = service.getBookingById("564231");
+//
+//        System.out.println(bookingGet);
+//
+//        bookingGet.setDate("2013-05-05");
+
+
     }
 }
